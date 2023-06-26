0a62dc2ca6ae2f6c1dfbe0735259513933d14de1
Nícolas Nascimento <77700068+NIKAO-CODE@users.noreply.github.com>
2023-06-26 11:10:44 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Qevmere, iy eqs zsgi. Pmrhe ❤</h1>
+  return <h1>Qevmere, iy eqs zsgi. Pmrhe ❤</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
